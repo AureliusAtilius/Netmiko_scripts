92aa8822dcf6bab075be0f0d718c5e5bacc881cb
Josh <j.j.h.lindner@gmail.com>
2021-06-25 18:04:49 -0700

    Correct username.

--- netmiko3.py
+++ netmiko3.py
@@ -4,21 +4,21 @@ from netmiko import ConnectHandler
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.255.83',
-    'username': 'david',
+    'username': 'josh',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.255.84',
-    'username': 'david',
+    'username': 'josh',
     'password': 'cisco',
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.255.85',
-    'username': 'david',
+    'username': 'josh',
     'password': 'cisco',
 }
 
